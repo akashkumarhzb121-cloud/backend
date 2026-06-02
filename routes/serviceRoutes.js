93d8242c2da38f14095f0da97e10dc6e648fdd49
Akash Kumar <akashkumarhzb121@gmail.com>
2026-06-02 12:34:46 +0530

    Update media upload handling for service routes

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { body } = require('express-validator');
-
 const serviceController     = require('../controllers/serviceController');
 const { protect, restrictTo } = require('../middleware/auth');
 const validate              = require('../middleware/validate');
@@ -21,8 +20,9 @@ router.get('/:id', serviceController.getService);
 // Admin only
 router.use(protect, restrictTo('admin', 'superadmin'));
 
-router.post('/', upload.single('image'), serviceValidation, validate, serviceController.createService);
-router.put('/:id', upload.single('image'), validate, serviceController.updateService);
+// Use array() to support multiple images + videos (unlimited)
+router.post('/', upload.array('media'), serviceValidation, validate, serviceController.createService);
+router.put('/:id', upload.array('media'), validate, serviceController.updateService);
 router.delete('/:id', serviceController.deleteService);
 
 module.exports = router;
